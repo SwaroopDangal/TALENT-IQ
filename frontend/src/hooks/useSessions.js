9935a518f5cb6d500113073ffcbf6f664b40b8a2
SwaroopDangal <swaroopdangal732@gmail.com>
2025-12-27 11:01:16 +0545

    session api and hooks created

---
+++ frontend/src/hooks/useSessions.js
@@ -0,0 +1,65 @@
+import { useMutation, useQuery } from "react-query";
+import { sessionApi } from "../api/sessions.js";
+import toast from "react-hot-toast";
+import { use } from "react";
+
+export const useCreateSession = () => {
+  const result = useMutation({
+    mutationKey: ["createSession"],
+    mutationFn: sessionApi.createSession,
+    onSuccess: () => toast.success("Session Created Successfully"),
+    onError: (error) =>
+      toast.success(
+        error?.response?.data?.message || "Failed to create session"
+      ),
+  });
+
+  return result;
+};
+
+export const useActiveSessions = () => {
+  const result = useQuery({
+    queryKey: ["activeSessions"],
+    queryFn: sessionApi.getActiveSessions,
+  });
+  return result;
+};
+export const useMyRecentSessions = () => {
+  const result = useQuery({
+    queryKey: ["myRecentSessions"],
+    queryFn: sessionApi.getMyRecentSessions,
+  });
+  return result;
+};
+export const useSessionById = (id) => {
+  const result = useQuery({
+    queryKey: ["session", id],
+    queryFn: () => sessionApi.getSessionById(id),
+    enabled: !!id,
+    refetchInterval: 5000,
+  });
+  return result;
+};
+
+export const useJoinSession = (id) => {
+  const result = useMutation({
+    mutationKey: ["joinSession"],
+    mutationFn: sessionApi.joinSession(id),
+    onSuccess: () => toast.success("Joined Session Successfully"),
+    onError: (error) =>
+      toast.error(error?.response?.data?.message || "Failed to join session"),
+  });
+
+  return result;
+};
+export const useEndSession = (id) => {
+  const result = useMutation({
+    mutationKey: ["endSession"],
+    mutationFn: sessionApi.endSession(id),
+    onSuccess: () => toast.success("Session Ended Successfully"),
+    onError: (error) =>
+      toast.error(error?.response?.data?.message || "Failed to create room"),
+  });
+
+  return result;
+};
